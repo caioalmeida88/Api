881194e1963cb6ee7841fdfbb093ed2555bda033
caioalmeida88 <134985458+caioalmeida88@users.noreply.github.com>
2023-06-18 22:18:39 -0300

    Trabalho A2.py

--- Trabalho A2.py
+++ Trabalho A2.py
@@ -4,13 +4,15 @@ import pandas as pd
 # Criar um dataframe de exemplo
 data = {
     'Anos': ['2019', '2020', '2021', '2022'],
-    'Gastos': [413205.44, 96832.90, 312646.21, 382404.09],
+    'Idade': [ 413205.44, 96832.90, 312646.21, 382404.09 ],
+    'Fornecedores': ['Tam', 'Gol', 'Omega', 'Celular Funcional']
 }
 
 df = pd.DataFrame(data)
 
 # Exibir a tabela
 st.table(df)
+
 
 
 
