fb6e282d6ed7b022a7d4f5662e1ead3326e6847f
caioalmeida88 <134985458+caioalmeida88@users.noreply.github.com>
2023-06-19 18:44:38 -0300

    Trabalho A2.py

--- Trabalho A2.py
+++ Trabalho A2.py
@@ -1,25 +1,49 @@
+import requests
 import streamlit as st
-import pandas as pd
 
-# Criar um dataframe de exemplo
-data = {
-    'Nome': ['João', 'Maria', 'Pedro', 'Ana'],
-    'Idade': [25, 30, 28, 35],
-    'Cidade': ['São Paulo', 'Rio de Janeiro', 'Curitiba', 'Porto Alegre']
-}
+def obter_gastos_por_parlamentar(nome_deputado):
+    url_base = "https://dadosabertos.camara.leg.br/api/v2/"
+    endpoint = f"deputados?nome={nome_deputado}"
+    url = url_base + endpoint
 
-df = pd.DataFrame(data)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            dados = response.json()
+            deputado_id = dados['dados'][0]['id']
+            endpoint_gastos = f"deputados/{deputado_id}/despesas?ano=2019,2020,2021,2022"
+            url_gastos = url_base + endpoint_gastos
+            response_gastos = requests.get(url_gastos)
+            if response_gastos.status_code == 200:
+                dados_gastos = response_gastos.json()
+                gastos_totais = sum(despesa['valorLiquido'] for despesa in dados_gastos['dados'])
+                return gastos_totais
+            else:
+                return None
+        else:
+            return None
+    except requests.exceptions.RequestException as e:
+        return None
 
-# Exibir a tabela
-st.table(df)
-import streamlit as st
-import numpy as np
+# Interface do Streamlit
+st.title("Consulta de Gastos por Parlamentar")
 
-# Criar uma matriz NumPy de exemplo
-data = np.random.randn(5, 3)
+# Entrada do nome do deputado
+nome_deputado = st.text_input("Digite o nome do deputado")
 
-# Exibir a tabela
-st.table(data)
+# Botão para consultar os gastos
+if st.button("Consultar Gastos"):
+    if nome_deputado:
+        gastos_parlamentar = obter_gastos_por_parlamentar(nome_deputado)
+        if gastos_parlamentar is not None:
+            st.write(f"Total de gastos do deputado {nome_deputado} entre 2019 e 2022: R$ {gastos_parlamentar:.2f}")
+        else:
+            st.write("Não foi possível obter os gastos do parlamentar.")
+    else:
+        st.write("Por favor, digite o nome do deputado.")
+
+
+
 
 
 
